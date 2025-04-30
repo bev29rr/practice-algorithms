7fc0b4fa30ae19e9cb784b0e30c2e5d84e4a3afc
tencalink0 <bencax270@gmail.com>
2025-04-30 22:31:17 +0100

    feat: update

--- web/main.ts
+++ web/main.ts
@@ -1,8 +1,40 @@
 import './modules/sorting.ts';
 import { Sort } from './modules/sorting.ts';
 
+const searcher = document.getElementById("searcher") as HTMLSelectElement | null;
+const sorter = document.getElementById("sorter") as HTMLSelectElement | null;
+const sorterText = document.getElementById("sorter-text");
+
+function randomArr(size: number): number[] {
+    const result: number[] = [];
+    for (let i = 0; i < size; i++) {
+        result.push(Math.random());
+    }
+    return result;
+}
+
+function timeAlg(fn: (arr: number[]) => number[], params: number[]): number {
+    const start = performance.now();
+    fn(params);
+    return performance.now() - start;
+}
+
+searcher?.addEventListener("change", () => {
+    if (searcher.value === "linear") {
+        if (sorter && sorterText) { 
+            sorter.disabled = true;
+            sorterText.style.color = 'var(--disabled)';
+        }
+      } else {
+        if (sorter && sorterText) { 
+            sorter.disabled = false;
+            sorterText.style.color = 'white';
+        }
+    }
+});
+
 document.addEventListener("DOMContentLoaded", () => {
     console.log("Sorting!");
-    console.log(Sort.insertion([8, 5, 4]));
+    //console.log(timeAlg(Sort.insertion, randomArr(100000)));
     console.log(Sort.merge([8, 5, 4]));
 });
