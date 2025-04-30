32f492028c92cc6c910d6fc899787376cbd38039
tencalink0 <bencax270@gmail.com>
2025-04-30 23:03:12 +0100

    feat: random question

--- web/main.ts
+++ web/main.ts
@@ -4,6 +4,16 @@ import { Sort } from './modules/sorting.ts';
 const searcher = document.getElementById("searcher") as HTMLSelectElement | null;
 const sorter = document.getElementById("sorter") as HTMLSelectElement | null;
 const sorterText = document.getElementById("sorter-text");
+const questionText = document.getElementById("question");
+
+const submitBtn = document.getElementById("submit") as HTMLButtonElement | null;
+let buttonState = 0;
+
+let questions: [string, () => void][] = [
+    ["A small array (n ≈ 1,000)", () => {}],
+    ["A large array (n ≈ 1,000,000)", () => {}],
+    ["A sorted large array (n ≈ 1,000,000)", () => {}]
+]
 
 function randomArr(size: number): number[] {
     const result: number[] = [];
@@ -37,4 +47,18 @@ document.addEventListener("DOMContentLoaded", () => {
     console.log("Sorting!");
     //console.log(timeAlg(Sort.insertion, randomArr(100000)));
     console.log(Sort.merge([8, 5, 4]));
+});
+
+submitBtn?.addEventListener("click", () => {
+    if (buttonState === 0) {
+        submitBtn.innerText = "Submit";
+        buttonState = 1;
+        let questionId = Math.floor(Math.random() * questions.length-1) + 1;
+        const question = questions[questionId];
+        const [q, fn] = question;
+
+        if (questionText) {
+            questionText.innerHTML = q;
+        }
+    }
 });
