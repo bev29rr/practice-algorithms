f4beeeba8a7914f0e2df0ab1d6dc9135fc70ef0a
tencalink0 <bencax270@gmail.com>
2025-05-01 18:51:24 +0100

    tests: fix

--- web/tests/searching.test.ts
+++ web/tests/searching.test.ts
@@ -3,20 +3,20 @@ import { Search } from "../modules/searching.ts";
 
 Deno.test("Search.linearSearch()", () => {
     const arr = [2, 4, 6, 8, 10];
-    assertEquals(Search.linearSearch(arr, 6), 2);
-    assertEquals(Search.linearSearch(arr, 2), 0);
-    assertEquals(Search.linearSearch(arr, 10), 4);
-    assertEquals(Search.linearSearch(arr, 5), -1);
+    assertEquals(Search.linear(arr, 6), 2);
+    assertEquals(Search.linear(arr, 2), 0);
+    assertEquals(Search.linear(arr, 10), 4);
+    assertEquals(Search.linear(arr, 5), -1);
 });
 
 Deno.test("Search.binarySearch()", () => {
     const arr = [1, 3, 5, 7, 9];
-    assertEquals(Search.binarySearch(arr, 5), 2);
-    assertEquals(Search.binarySearch(arr, 1), 0);
-    assertEquals(Search.binarySearch(arr, 9), 4);
-    assertEquals(Search.binarySearch(arr, 6), -1);
+    assertEquals(Search.binary(arr, 5), 2);
+    assertEquals(Search.binary(arr, 1), 0);
+    assertEquals(Search.binary(arr, 9), 4);
+    assertEquals(Search.binary(arr, 6), -1);
 
-    assertEquals(Search.binarySearch([], 1), -1);
-    assertEquals(Search.binarySearch([3], 3), 0);
-    assertEquals(Search.binarySearch([3], 1), -1);
+    assertEquals(Search.binary([], 1), -1);
+    assertEquals(Search.binary([3], 3), 0);
+    assertEquals(Search.binary([3], 1), -1);
 });
